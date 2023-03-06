6a6d6971ee3ce9181693df9e8a3fe6db5ae6a7f6
Areeb Khalfay <areebkhalfay@Areebs-MacBook-Pro.local>
2023-03-06 00:29:01 -0800

    Initial Commit for Laravel Job Post Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         User::factory(10)->create();
+
+         Listing::factory(6)->create();
+
+//         Listing::create([
+//             'title' => 'Senior Software Engineer',
+//             'tags' => 'PHP, Javascript, React, Laravel',
+//             'company' => 'Salesforce',
+//             'location' => 'Atlanta, GA',
+//             'email' => 'email1@test.com',
+//             'website' => 'https://www.salesforce.com',
+//             'description' => 'An opportunity to join Salesforce as a Senior Software Engineer. Salesforce, Inc. is an American cloud-based software company headquartered in San Francisco, California. It provides customer relationship management software and applications focused on sales, customer service, marketing automation, e-commerce, analytics, and application development.'
+//         ]);
+//
+//         Listing::create([
+//             'title' => 'Software Engineer',
+//             'tags' => 'Swift, Java, MacOS',
+//             'company' => 'Apple',
+//             'location' => 'Cupertino, CA',
+//             'email' => 'email2@test.com',
+//             'website' => 'https://www.apple.com',
+//             'description' => 'An opportunity to join Apple Inc. as a Software Engineer. Apple Inc. is an American multinational technology company headquartered in Cupertino, California, United States.'
+//         ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//All Listings
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+//Single Listing
+//Does 404 on its own
+Route::get("/listings/{listing}", function(Listing $listing) {
+    return view('listing', [
+        'listing' => $listing
+    ]);
+});
+
+//Route::get("/hello", function() {
+//    return response("<h1>Hello World</h1>", 200)
+//        ->header("Content-Type", "text/plain")
+//        ->header("foo", "bar");
+//});
+//
+//Route::get("/posts/{id}", function($id) {
+//    return response("Post " . $id);
+//})->where('id', "[0-9]+");
+//
+//Route::get("/search", function(Request $request) {
+//    return$request->name . ' ' . $request->city;
+//});
