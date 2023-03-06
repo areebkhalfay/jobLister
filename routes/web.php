df98aff84d24efe4d40a704cfd4ed93b5ff32e17
Areeb Khalfay <areebkhalfay@Areebs-MacBook-Pro.local>
2023-03-06 00:57:16 -0800

    Removed unnecessary comments

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,3 @@ Route::get("/listings/{listing}", function(Listing $listing) {
         'listing' => $listing
     ]);
 });
-
-//Route::get("/hello", function() {
-//    return response("<h1>Hello World</h1>", 200)
-//        ->header("Content-Type", "text/plain")
-//        ->header("foo", "bar");
-//});
-//
-//Route::get("/posts/{id}", function($id) {
-//    return response("Post " . $id);
-//})->where('id', "[0-9]+");
-//
-//Route::get("/search", function(Request $request) {
-//    return$request->name . ' ' . $request->city;
-//});
